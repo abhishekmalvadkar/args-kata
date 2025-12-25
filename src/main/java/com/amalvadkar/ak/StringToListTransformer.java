0b65daa188fa9ec5be9791e99e4cd92dad89f759
Abhishek <abhi@dev.com>
2025-12-25 12:48:43 +0530

    Just refactoring

--- src/main/java/com/amalvadkar/ak/StringToListTransformer.java
+++ src/main/java/com/amalvadkar/ak/StringToListTransformer.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class StringToListTransformer {
+
+    private static final String LIST_FLAG_VALUE_SEPARATOR = ",";
+
     public static List<String> transform(String value) {
-        return Stream.of(value.split(","))
+        return Stream.of(value.split(LIST_FLAG_VALUE_SEPARATOR))
                 .toList();
     }
 }
