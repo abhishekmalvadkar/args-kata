d9c382f21b0ddcf2d884a8c330092995bd57b8b3
Abhishek <abhi@dev.com>
2025-12-28 15:12:05 +0530

    Converted application file to compact source file

--- src/main/java/com/amalvadkar/ak/Application.java
+++ src/main/java/com/amalvadkar/ak/Application.java
@@ -1,16 +1,12 @@
-package com.amalvadkar.ak;
+import com.amalvadkar.ak.Argument;
+import com.amalvadkar.ak.InvalidFlagException;
+import com.amalvadkar.ak.InvalidFlagValueException;
 
-import java.util.List;
-
-public class Application {
-
-	static void main(String[] args) {
-        try {
-            Argument argument = Argument.parse(List.of(args));
-            IO.println(argument);
-        } catch (InvalidFlagException | InvalidFlagValueException e) {
-			IO.print(e.getMessage());
-        }
+void main(String[] args) {
+    try {
+        Argument argument = Argument.parse(List.of(args));
+        IO.println(argument);
+    } catch (InvalidFlagException | InvalidFlagValueException e) {
+        IO.print(e.getMessage());
     }
-
 }
